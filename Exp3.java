0d48ba7edde017d03475b5382f28b3bf96e28674
Atmaj Koppikar <67873291+Atlas28625@users.noreply.github.com>
2022-02-03 19:16:50 +0530

    Added Exp3 on EE and MI algorithm

---
+++ Exp3.java
@@ -0,0 +1,7 @@
+/* Extended Euclidian Algorithm
+To find gcd of a number along with a bunch of useless shit
+that only exists because we need to find the distance between
+letters in a plaintext or some shit like that and because it
+comes in the exam for 6-8 marks in alot of papers.
+*/
+
